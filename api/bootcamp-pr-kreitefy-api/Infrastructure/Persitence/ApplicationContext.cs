176f8b7ed06124d4a03f67b7e99003b0597a2e9f
Angel Hernandez <angelillohdez@gmail.com>
2024-11-25 15:50:33 +0100

    feat(api): database configured

---
+++ api/bootcamp-pr-kreitefy-api/Infrastructure/Persitence/ApplicationContext.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace bootcamp_pr_kreitefy_api.Infrastructure.Persistence
+{
+    public class ApplicationContext : DbContext
+    {
+
+        public ApplicationContext(DbContextOptions<ApplicationContext> options) : base(options) { }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            base.OnConfiguring(optionsBuilder);
+        }
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+        }
+    }
+}
